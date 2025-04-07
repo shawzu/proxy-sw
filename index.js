e2eff1e197671cec0d1d258098dd2d7f65862301
shawzu <ilyass.oumlil@yahoo.com>
2025-04-07 22:10:54 +0200

    increase rate limit

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const app = express();
 // Rate limiting to prevent abuse
 const limiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 1500, // limit each IP to 100 requests per windowMs
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   message: { error: 'Too many requests, please try again later.' }
